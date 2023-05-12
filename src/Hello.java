d5012e01179c51b58f9538a631e79ca68cef99ee
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-12 13:15:18 +0530

    mohanverse

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("Hello java");
         System.out.println("Hello Mohan");
     }
 }
