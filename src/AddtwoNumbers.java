3909cacd987cdba1e7933585b3b67cb69aebe953
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-13 01:37:50 +0530

    mohanverse

---
+++ src/AddtwoNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AddtwoNumbers {
+    public static void main(String[] args){
+        int myFirstNum;
+        int mySecNum;
+        int result;
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter num 1: ");
+        myFirstNum = in.nextInt();
+
+        System.out.print("Enter num 2: ");
+        mySecNum = in.nextInt();
+
+        System.out.print("your added value is: ");
+        result = myFirstNum + mySecNum;
+
+        System.out.println(result);
+    }
+}
