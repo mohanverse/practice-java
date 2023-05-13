f5eae52ef733cfd8d0da11bee58766f7ba57a7c1
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-13 09:53:43 +0530

    mohanverse

---
+++ src/DataTypes.java
@@ -0,0 +1,18 @@
+//data types 
+//byte - -128 : 127
+//short - -32768 : 32767
+//int - -2, 147, 484, 648
+//long - 9, 223, 372, 036, 854, 775, 775, 808
+//float - 231.2f
+//double - 231.2f
+//boolean - true/false
+//char - 'Z'
+//string - 'mohan'
+
+public class DataTypes {
+
+    public static void main(String[] args){
+        System.out.println("new module");
+    }
+    
+}
