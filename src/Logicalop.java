881aa75b792cda8d18985726c1b596fab9a84a59
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-14 01:28:08 +0530

    mohanverse

---
+++ src/Logicalop.java
@@ -0,0 +1,11 @@
+public class Logicalop {
+    public static void main(String args[]){
+
+        boolean hasAcount = true;
+        boolean isLLoggedIn = false;
+        //boolean hasPaid = false;
+
+        System.out.println( hasAcount && isLLoggedIn );
+        System.out.println( hasAcount ||  isLLoggedIn );
+    }
+}
