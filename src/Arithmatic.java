06b04a8535158654fd69480af014b9961f44ff3f
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-14 01:04:19 +0530

    mohanverse

--- src/Arithmatic.java
+++ src/Arithmatic.java
@@ -3,9 +3,9 @@ public class Arithmatic {
     public static void main(String[] args){
         int numBer1= 10;
         int numBer2= 20;
-        int numBer3= 30;
+        int numBer3= 35;
 
-        System.out.println(numBer3/numBer1);
+        System.out.println(numBer3 % numBer1);
 
     }
     
