6a0fdb034f4b2a2da3a01ac8f6de44e94b8b98d0
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-14 01:28:23 +0530

    mohanverse

--- src/Relational.java
+++ src/Relational.java
@@ -1,7 +1,14 @@
 public class Relational {
 
     public static void main (String args[]){
-        
+        int num1 = 10;
+        int num2 = 15;
+
+        System.out.println(num1 == num2);
+        //false
+        System.out.println(num1 != num2);
+        //true
+
     }
     
 }
