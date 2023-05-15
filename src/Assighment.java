30a6a865354e2a35492a87fcf04e5ba50f2b31ee
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-15 11:13:31 +0530

    mohanverse

---
+++ src/Assighment.java
@@ -0,0 +1,12 @@
+public class Assighment {
+    public static void main(String args[]) {
+        int number1 = 10;
+        int number2 = 5;
+
+        int number3 = number1;
+        System.out.println(number3);
+        
+    }
+    
+    
+}
