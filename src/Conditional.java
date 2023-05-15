e5d71480f4d2a9e66d61ea1b7d120da3e0c7a23b
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-15 14:22:50 +0530

    mohanverse

---
+++ src/Conditional.java
@@ -0,0 +1,16 @@
+public class Conditional {
+    public static void main(String args[]) {
+
+        int temp= 35;
+        if(temp < 30){
+            System.out.println("temp is less then 30");
+        } if(temp==35){
+            System.out.println("temp is less then = 35");
+        }else{
+            System.out.println("temp is hot");
+        }
+
+        
+    }
+    
+}
