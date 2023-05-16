ec25aabc566880de62e5761bc7a746799881f6c1
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-16 20:12:15 +0530

    mohanverse

---
+++ src/Loops.java
@@ -0,0 +1,40 @@
+public class Loops {
+
+    //for loop
+
+    // public static void main(String args[]) {
+    //     for (int i = 0; i <= 5; i++) {
+    //         System.out.println(i);
+    //       }
+        
+    // }
+
+    //while loop
+    // public static void main(String args[]) {
+    //     int i = 0;
+    //     while (i <= 5) {
+    //         System.out.println(i);
+    //         i++;
+    //     }
+        
+    // }
+
+
+    //do while loop
+
+    public static void main(String args[]) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 5);
+        
+    }
+
+
+
+    
+
+    
+}
