073ea8d0c70d510e8efed1bdc4872c00361b119d
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-19 17:04:39 +0530

    mohanverse

---
+++ src/FindAnumber.java
@@ -0,0 +1,24 @@
+//list of even numbers till 200.
+
+
+public class FindAnumber {
+    public static void main(String args[]) {
+
+        int upperLimit = 200;
+
+        for (int i = 1; i<=200; i++){
+            // System.out.println("Number is " + i);
+
+            // for even numbers
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+            //for odd numbers
+            // if(i%2 != 0){
+            //     System.out.println(i);
+            // }
+        }
+        
+    }
+    
+}
