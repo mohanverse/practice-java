d9015cab5b0a27d67e78dbca6fde7342e1f5ec78
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-19 16:53:39 +0530

    mohanverse

--- src/LogicOpration.java
+++ src/LogicOpration.java
@@ -2,5 +2,24 @@
 //compare two numbers 
 
 public class LogicOpration {
+    public static void main(String args[]) {
+
+        //declaire a two variable
+        int nuberOne = 400;
+        int numberTwo = 400;
+
+        if(nuberOne > numberTwo){
+            System.out.println(nuberOne + "is greater than " +numberTwo);
+        }
+        else if(nuberOne < numberTwo){
+            System.out.println(nuberOne + "is less than" +numberTwo);
+
+        }
+        else{
+            System.out.println(nuberOne + " is equil to " +numberTwo);
+        }
+
+        
+    }
     
 }
