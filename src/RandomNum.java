b6e1211ec15d7ee360d2da810ccb08f9f49b0358
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-20 19:30:37 +0530

    mohanverse

--- src/RandomNum.java
+++ src/RandomNum.java
@@ -1,3 +1,13 @@
+// Print a random numbers 1 To 100
+
 public class RandomNum {
-    
+    public static void main(String args[]) {
+        for(int i = 1; i<=5; i++){
+
+            System.out.println( (int) (Math.random()*100) );
+
+        }
+        
+    }
+
 }
