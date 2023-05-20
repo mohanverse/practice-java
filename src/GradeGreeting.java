39c4a58b330d12884ff18883e9bc7b4bbaef7a7f
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-20 20:14:07 +0530

    mohanverse

---
+++ src/GradeGreeting.java
@@ -0,0 +1,32 @@
+public class GradeGreeting {
+
+    public static void main( String args[]) {
+
+        char grade = 'B';
+        switch(grade){
+            case 'A':
+               System.out.println("Amazing work there keep going");
+               break;
+            case 'B':
+               System.out.println("Amazing work there keep going");
+               break;
+            case 'C':
+               System.out.println("Amazing work there keep going");
+               break;
+            case 'D':
+               System.out.println("Amazing work there keep going");
+               break;
+            case 'E':
+               System.out.println("Amazing work there keep going");
+               break;
+            case 'F':
+               System.out.println("Amazing work there keep going");
+               break;
+            default:
+            System.out.println("grade Can only be A to F");   
+
+        }
+        
+    }
+    
+}
