7e93429e7c1d79fb007b5376169e8ff8c60c80a2
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-20 16:42:16 +0530

    mohanverse

--- src/PatternA.java
+++ src/PatternA.java
@@ -1,8 +1,24 @@
+// Print a star
+
 public class PatternA {
     public static void main(String args[]) {
+        //Pattern a
 
-        for (int i = 1; i<=5; i++){
-            for(int j = 0; j<i; j++){
+
+        // for (int i = 1; i<=5; i++){
+        //     for(int j = 0; j<i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        
+        //pattern b
+
+
+        for( int i = 5; i > 0; i--){
+            for( int j=0; j<i; j++){
                 System.out.print("*");
             }
             System.out.println("");
