524ed3e8126f1e9f016aff73f473c01c71e6b485
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-05-20 16:52:11 +0530

    mohanverse

--- src/PatternA.java
+++ src/PatternA.java
@@ -13,16 +13,28 @@ public class PatternA {
         // }
 
 
-        
+
         //pattern b
 
 
-        for( int i = 5; i > 0; i--){
-            for( int j=0; j<i; j++){
-                System.out.print("*");
+        // for( int i = 5; i > 0; i--){
+        //     for( int j=0; j<i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        // Pattern c
+
+         for( int i = 5; i > 0; i--){
+            for(int j=0; j<i; j++){
+                System.out.print(j+1);
             }
             System.out.println("");
         }
+        
+
         
     }
     
